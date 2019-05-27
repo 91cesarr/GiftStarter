7af9b11337143071387675bfed42961f51a1ee8d
Cesar <cesarlogin@outlook.com>
2019-05-27 16:29:40 -0700

    final cleanup

--- client/src/components/DashData.jsx
+++ client/src/components/DashData.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useContext } from "react";
-import { ResponsiveContainer, ComposedChart, Line, Area, XAxis, YAxis, CartesianGrid, Tooltip} from "recharts";
 import PropTypes from "prop-types";
 import moment from "moment"
 import { getUser, getItems, getDonList } from "../actions/actions"
@@ -31,13 +30,14 @@ const DashData = props => {
     getItems(props.userData.user_id)
     getDonList(props.item.item_id)
   }, [user, props.userData.user_id, props.item.item_id])
+  console.log(props.donations)
   const { classes } = props;
   return (
     <div>
       <GridContainer justify="center">
         <GridItem xs={12} sm={12} md={12}>
-          <Card>
-            <CardHeader color="primary">
+          <Card className="dash-card">
+            <CardHeader color="info">
               <h4 className={classes.cardTitleWhite}>My Wishes</h4><br />
               <p className={classes.cardCategoryWhite}>
                 Requested items as of&nbsp;
@@ -45,37 +45,8 @@ const DashData = props => {
               </p>
             </CardHeader>
             <CardBody>
-              {/* start chart data */}
-              <div className="chart_wrapper" style={{ width: "100%", height: 300 }}>
-                <ResponsiveContainer>
-                  <ComposedChart
-                    width={500}
-                    height={400}
-                    data={props.donations}
-                    margin={{
-                      top: 20,
-                      right: 20,
-                      bottom: 20,
-                      left: 20
-                    }}
-                  >
-                    <CartesianGrid stroke="#f5f5f5" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Area
-                      type="monotone"
-                      dataKey="amount"
-                      fill="#26c6da"
-                      stroke="#26c6da"
-                    />
-                    <Line type="monotone" dataKey={props.donations.amount} stroke="#11998e" />
-                  </ComposedChart>
-                </ResponsiveContainer>
-              </div>
-              {/* end chart data */}
               <Table
-                tableHeaderColor="primary"
+                tableHeaderColor="info"
                 tableHead={["ID", "Name", "Amount", "Donated", "Remaining"
                   // , "Status"
                 ]}
